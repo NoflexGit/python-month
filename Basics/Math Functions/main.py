bb00185c81040013a615fc23315187e3e5757255
NoflexGit <untroman@mail.ru>
2021-10-28 02:31:00 +0300

    Basics. Number and Math Functions

---
+++ Basics/Math Functions/main.py
@@ -0,0 +1,8 @@
+# Math functions
+
+import math
+
+print(round(3.1))
+print(abs(-20))
+print(math.floor(12.90))
+print(math.pi)
---
+++ Basics/Numbers/main.py
@@ -0,0 +1,14 @@
+# Ints
+print(type(2 + 4)) 
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+
+#Floats
+print(type(2 / 4)) 
+print(20 + 4.44) 
+print(9.9 + 1.1) 
+
+print(2 ** 3)
+print(3 // 4)
+print(5 % 4)
