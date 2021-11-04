7c05bc0d4e57aecf6cfe6a39987a29782f7ede13
NoflexGit <untroman@mail.ru>
2021-11-04 22:08:48 +0300

    Continuing with OOP

--- 03. OOP/01. Create Own Class/main.py
+++ 03. OOP/01. Create Own Class/main.py
@@ -1,4 +1,6 @@
 class Car:
+    couldBeElectroCar = True
+
     def __init__(self, brand, model, year) -> None:
         self.brand = brand
         self.model = model
@@ -7,7 +9,11 @@ class Car:
     def show_model(self):
         print(self.model)
 
+    def drive(self):
+        print(f'I drive {self.brand} {self.model} {self.year}')
+
 
 tesla = Car('Tesla', 'Model S', 2021)
 
 tesla.show_model()
+tesla.drive()
---
+++ 03. OOP/02. Private and Public Methods/main.py
@@ -0,0 +1,12 @@
+class Animal:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def __run(self):
+        print(self.model)
+
+    def __eat(self):
+        print(f'')
+
+    def __swim(self):
+        print(f'{self.name} is swimming')
